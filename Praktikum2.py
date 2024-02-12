8e499744fc5bafcd945279c300e18621b744e40c
Karasik07 <aleksandr.rasskazov.2019@gmail.com>
2024-02-12 15:24:01 +0300

    Расчет энтропии

---
+++ Praktikum2.py
@@ -0,0 +1,13 @@
+import math
+text = input("Введите текст: ")
+A = len(set(text.lower()))
+print("Мощность алфавита = ", A)
+infHartly = math.log(A,2)
+print("Расчет информационной энтропии по Хартли: ", infHartly)
+infShennonAVG = 0.0; ver = 0.0
+for val in set(text.lower()):
+    ver = text.count(val) / A
+    infShennonAVG = infShennonAVG - (ver * math.log(ver,2))
+print("Расчет информационной энтропии по Шеннону по формуле: ", infHartly)
+D = ((infHartly - infShennonAVG) / infHartly) * 100
+print("Расчет избыточности алфавита: ", D)
