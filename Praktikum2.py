2e9a027fc6696b2840977989a55df33c2987e4e2
Karasik07 <aleksandr.rasskazov.2019@gmail.com>
2024-02-12 15:37:04 +0300

    Расчет энтропии

--- Praktikum2.py
+++ Praktikum2.py
@@ -6,7 +6,7 @@ infHartly = math.log(A,2)
 print("Расчет информационной энтропии по Хартли: ", infHartly)
 infShennonAVG = 0.0; ver = 0.0
 for val in set(text.lower()):
-    ver = text.count(val) / A
+    ver = text.count(val) / len(text)
     infShennonAVG = infShennonAVG - (ver * math.log(ver,2))
 print("Расчет информационной энтропии по Шеннону по формуле: ", infHartly)
 D = ((infHartly - infShennonAVG) / infHartly) * 100
